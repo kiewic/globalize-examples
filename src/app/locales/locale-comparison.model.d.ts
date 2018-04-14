fa6c8a42c54cfdde713774db63a7a1be17ad61b8
Gilberto Stankiewicz <hello@kiewic.com>
2018-04-13 22:29:30 -0700

    Display locales by "Same name", "Changed name", "New locales" and "Not available".

--- src/app/locales/locale-comparison.model.d.ts
+++
@@ -1,8 +0,0 @@
-// TODO: Is it ok to have only an export interface in a file? Why did I had to add `*.d.ts` extension?
-export interface LocaleComparisons {
-    [id: string]: {
-        version0?: boolean;
-        modern?: boolean;
-        full?: boolean;
-    }
-}
